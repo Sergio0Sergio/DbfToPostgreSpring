7acf20dff402524414a071cdb414989d70a1702a
Sergei Grimanov <tosergi@gmail.com>
2017-08-28 17:21:09 +0300

    First commit

---
+++ src/main/java/ru/habrahabr/sergiosergio/DbfToPostgreSpringApplication.java
@@ -0,0 +1,12 @@
+package ru.habrahabr.sergiosergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbfToPostgreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbfToPostgreSpringApplication.class, args);
+	}
+}
